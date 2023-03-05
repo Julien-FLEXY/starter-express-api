becb5866c10f068f0f2c6cdd3f91580299664faf
julien <julien@flexy-web.com>
2023-03-05 16:42:29 +0100

    save

--- index.js
+++ index.js
@@ -1,17 +1,43 @@
-const express = require('express')
-const app = express()
+import express from 'express';
 
+const shop = "julien-test-site";
+const baseURL = `https://${shop}.myshopify.com/`;
+const productVariantId = "39763716341825";
+const apiEndpoint = `/admin/api/2023-01/variants/${productVariantId}.json`;
+
+const app = express();
+
+// Update product variant price
 app.all('/', (req, res) => {
-    fetch("https://julien-test-site.myshopify.com/admin/api/2023-01/products.json", {
-		method: 'GET',
-		headers: {
-			'Content-Type': 'application/json',
-			'X-Shopify-Access-Token': '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',
-		}
-	})
+    fetch(baseURL + apiEndpoint, {
+        method: 'GET',
+        headers: {
+            'Content-Type': 'application/json',
+            'X-Shopify-Access-Token': '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',
+        }
+    })
         .then(response => response.json())
-        .then(data => res.send(data))
-        .catch(err => console.log(err));
-})
+        .then(data =>  {
+            if (data.variant) {
+                const price = (parseInt(data.variant.price) + 1) + ".00";
+                const requestOptions = {
+                    method: 'PUT',
+                    headers: {
+                        'Content-Type': 'application/json',
+                        'X-Shopify-Access-Token': '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',
+                    },
+                    body: `{"variant":{"price":${price}}}`
+                };
 
-app.listen(process.env.PORT || 3000)
+                fetch(baseURL + apiEndpoint, requestOptions)
+                    .then(response => response.json())
+                    .then(data => res.send(data));
+            } else {
+                res.send("<p>No data</p>");
+            }
+        })
+        .catch(err => console.log(err));
+});
+
+// Start server
+app.listen(process.env.PORT || 3000, () => console.log("Server started"));
