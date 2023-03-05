e32f4ab96ce2b369aa94df4df884fe457a05d09a
julien <julien@flexy-web.com>
2023-03-05 15:12:45 +0100

    connect to API

--- index.js
+++ index.js
@@ -2,8 +2,15 @@ const express = require('express')
 const app = express()
 
 app.all('/', (req, res) => {
-    console.log("Just got a request Wow!")
-    res.send('Yo!')
+    fetch("https://julien-test-site.myshopify.com/admin/api/2023-01/products.json", {
+		method: 'GET',
+		headers: {
+			'Content-Type': 'application/json',
+			'X-Shopify-Access-Token': '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',
+		}
+	})
+        .then(data => res.send(data))
+        .catch(err => console.log(err));
 })
 
 app.listen(process.env.PORT || 3000)
